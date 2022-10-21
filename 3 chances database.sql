0f6862f6cb3d4d48568613b6e02ffaeb091bc9ff
Thiago_Cruz <101190863+mistickesterio@users.noreply.github.com>
2022-10-21 10:30:54 -0300

    second commit

--- 3 chances database.sql
+++ 3 chances database.sql
@@ -1,42 +1,32 @@
-create DATABASE Fornecedor_alimenticio;
-USE Fornecedor_alimenticio;
+create DATABASE Jogo;
+USE Jogo;
 
-CREATE TABLE Cliente(
-    ID INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE Jogador(
+    Acertos INT NOT NULL AUTO_INCREMENT,
     Nome VARCHAR(255),
-    CPF VARCHAR(11),
+    Tempo_Gasto VARCHAR(11),
     PRIMARY KEY (ID)
 );
 
-CREATE TABLE Produto(
-    ID VARCHAR(2) NOT NULL,
-    Nome VARCHAR(255),
-    PRIMARY KEY(iD)
+CREATE TABLE Pontuacao(
+    ID INT NOT NULL AUTO_INCREMENT,
+    Jogador VARCHAR(255),
+    Acertos VARCHAR(11),
+    Tempo_Gasto VARCHAR(11),
+    PRIMARY KEY (ID),
+    FOREIGN KEY (AcertosID) REFERENCES Acertos(ID)
 );
 
-CREATE TABLE Atendimento(
+CREATE TABLE Informacaos(
     ID INT NOT NULL AUTO_INCREMENT,
     Local VARCHAR(255),
     Data DATETIME,
-    ClienteID INT NOT NULL,
-    IDProduto VARCHAR(2) NOT NULL,
+    Comida VARCHAR(255),
+    Cor VARCHAR(255),
+    Lugar VARCHAR(255),
+    jogadorID INT NOT NULL,
+    IDPontuação VARCHAR(2) NOT NULL,
     PRIMARY KEY (ID),
-    FOREIGN KEY (ClienteID) REFERENCES Cliente(ID),
+    FOREIGN KEY (JogadorID) REFERENCES Jogadador(ID),
     FOREIGN KEY(IDProduto) REFERENCES Produto(ID)
 );
-
-INSERT INTO Cliente (Nome,CPF) VALUES
-('Lucas', '12312312312'),
-('Luiz', '32132132132'),
-('Lourival', '12312312312'),
-('Otavio', '32132132132'),
-('Kerolaine', '32132132132'),
-('Thiago', '34534534534');
-
-INSERT INTO Produto (Nome,ID) VALUES
-('Pipoca', '01'),
-('Docê de Leite', '02'),
-('Manteiga', '03'),
-('Pastél', '04'),
-('Coca Cola', '05'),
-('Cachorro quente', '06');
